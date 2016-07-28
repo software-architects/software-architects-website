bf140122bda01bd74b430b38a905c792d875e806
Karin Huber <karin@timecockpit.com>
2016-07-28 16:38:44 +0200

    -

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+
+gulp.task("default", ["copyBootstrapFiles","buildTypescript"], function () {
+    // place code for your default task here
+});
+
+gulp.task("copyBootstrapFiles", function () {
+	gulp.src("./node_modules/bootstrap-sass/assets/fonts/bootstrap/*.*")
+		.pipe(gulp.dest("./fonts/bootstrap"));
+
+	gulp.src("./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js")
+		.pipe(gulp.dest("./scripts"));
+
+	gulp.src("./node_modules/jquery/dist/jquery.js")
+		.pipe(gulp.dest("./scripts"));
+
+	return gulp.src("./node_modules/bootstrap-sass/assets/stylesheets/**/*.*")
+		.pipe(gulp.dest("./_sass/bootstrap"));
+});
+
+gulp.task("buildTypescript", function () {
+    return gulp.src("typescript/*.ts")
+        .pipe(ts({
+            noImplicitAny: true,
+            out: "software-architects-website.js"
+        }))
+        .pipe(gulp.dest("scripts"));
+});
---
+++ scripts/software-architects-website.js
@@ -0,0 +1,57 @@
+function subscribeToNewsletterWithEmail(email) {
+    ga("_trackEvent", "Newsletter", "Subscribe to newsletter time cockpit");
+    if (isEmailValid(email)) {
+        var input = { email: { email: email } };
+        $.ajax({
+            method: "GET",
+            url: "https://timecockpit.us5.list-manage.com/subscribe/post-json?u=f628a14a6f934bf75e14ef1c3&id=f4e09e442c&EMAIL=" + email + "&c=?",
+            dataType: "jsonp",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                if (data["result"] != "success") {
+                    alert(data["msg"]);
+                }
+                else {
+                    alert("Thanks for your registration! You are almost finished. We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you.");
+                }
+                var input = $("#newletterEmail,#mce-EMAIL");
+                input.each(function (element) {
+                    this.value = "";
+                });
+            }
+        });
+    }
+    else {
+        var error = document.getElementById("mce-ERROR");
+        if (error) {
+            error.value = "Please enter a valid email address.";
+        }
+        else {
+            alert("Please enter a valid email address.");
+        }
+    }
+}
+function subscribeToNewsletter() {
+    var email = document.getElementById("mce-EMAIL");
+    subscribeToNewsletterWithEmail(email.value);
+}
+function isEmailValid(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    if (email && re.test(email)) {
+        return true;
+    }
+    return false;
+}
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments);
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-3324842-1', 'auto');
+ga('send', 'pageview');
